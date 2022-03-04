c96e39fce87f54871692258ab67a6dec1d996096
Nguyen Trong Dat <49813515+NGUYENTRONGDAT123@users.noreply.github.com>
2022-03-04 14:05:51 +1000

    fixing the cypress
    making compatible with Ubuntu

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,16 +1,16 @@
 describe('Aperture Science Enhancement Center Cypress Tests', () => {
   it('finds the home page', function () {
-    cy.visit('http://host.docker.internal:3000')
+    cy.visit('http://172.17.0.1:3000')
     cy.get('h1');
   })
 
   it('cant find the subjects page without logging in', function () {
-    cy.visit('http://host.docker.internal:3000/subjects')
+    cy.visit('http://172.17.0.1:3000/subjects')
     cy.location('pathname').should('eq', '/')
   })
 
   it('can log in and see a list of subjects', function () {
-    cy.visit('http://host.docker.internal:3000')
+    cy.visit('http://172.17.0.1:3000')
     cy.get('#email').type(Cypress.env('email'));
     cy.get('#password').type(Cypress.env('password'));
 
@@ -19,7 +19,7 @@ describe('Aperture Science Enhancement Center Cypress Tests', () => {
     cy.location('pathname').should('eq', '/subjects')
     cy.get('h1').contains('Testing Subjects');
 
-    cy.intercept('POST', 'http://host.docker.internal/graphql').as('graphql')
+    cy.intercept('POST', 'http://172.18.0.1/graphql').as('graphql')
 
     cy.get('h1').contains('Testing Subjects');
     cy.get('div[data-testid="skeleton"]');
@@ -28,7 +28,31 @@ describe('Aperture Science Enhancement Center Cypress Tests', () => {
     cy.get('table[data-testid="subjects-table"]');
   });
 
-  it('can request subjects', function() {
+  // it('can request subjects', function() {
+  //   const query = `
+  //     query {
+  //       subjects {
+  //         id
+  //         name
+  //         test_chamber
+  //         date_of_birth
+  //         score
+  //         alive
+  //         created_at
+  //       }
+  //     }
+  //   `
+  //   cy.request({
+  //     method: 'POST', 
+  //     url: 'http://172.17.0.1/graphql', 
+  //     body: { query }
+  //   }).then((res) => {
+  //     cy.log(res.body)
+  //     expect(res.body, 'response body').property('data').property('subjects').to.have.lengthOf(10)
+  //   })
+  // });
+
+  it('cant request subjects without the authentication', function() {
     const query = `
       query {
         subjects {
@@ -44,11 +68,11 @@ describe('Aperture Science Enhancement Center Cypress Tests', () => {
     `
     cy.request({
       method: 'POST', 
-      url: 'http://host.docker.internal/graphql', 
+      url: 'http://172.18.0.1/graphql', 
       body: { query }
     }).then((res) => {
       cy.log(res.body)
-      expect(res.body, 'response body').property('data').property('subjects').to.have.lengthOf(10)
+      expect(res.body, 'response body').property('errors').to.exist
     })
   });
 
@@ -63,7 +87,7 @@ describe('Aperture Science Enhancement Center Cypress Tests', () => {
     `
     cy.request({
       method: 'POST', 
-      url: 'http://host.docker.internal/graphql', 
+      url: 'http://172.18.0.1/graphql', 
       body: { query }
     }).then((res) => {
       cy.log(res.body)
